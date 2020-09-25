bf9916c58832b088aebb58bb3c40c09c2c805346
Matheus <matheus.ramos.silva.santos@gmail.com>
2020-09-25 00:20:08 -0300

    Customers Finished

--- src/modules/customers/infra/http/controller/CustomersController.ts
+++ src/modules/customers/infra/http/controller/CustomersController.ts
@@ -6,6 +6,15 @@ import { container } from 'tsyringe';
 
 export default class CustomersController {
   public async create(request: Request, response: Response): Promise<Response> {
-    // TODO
+    const { name, email } = request.body;
+
+    const CreateCustomer = container.resolve(CreateCustomerService);
+
+    const customer = await CreateCustomer.execute({
+      email,
+      name,
+    });
+
+    return response.json(customer);
   }
 }
